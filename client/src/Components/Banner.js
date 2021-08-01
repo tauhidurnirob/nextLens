a1010655421ff74505f3f44fc05bfb3dca8031db
Shakiba448 <shakiba448@gmail.com>
2021-08-01 16:46:51 +0600

    WIP on Banner

--- client/src/Components/Banner.js
+++ client/src/Components/Banner.js
@@ -1,25 +1,13 @@
 import React from "react";
-import { Container, makeStyles, Grid } from "@material-ui/core";
-import clsx from "clsx";
-import Slider from "react-slick";
 import Image from "next/image";
 import img1 from "../assets/c1.png";
 import img2 from "../assets/c2.png";
 import img3 from "../assets/c3.png";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Carousel from "./Carousel";
+import Slider from "react-slick";
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    padding: `${theme.spacing(8)}px 0`,
-    overflow: "hidden",
-    width: "100%",
-  },
-}));
 
 const Banner = () => {
-  const classes = useStyles();
-
   const settings = {
     dots: true,
     infinite: true,
@@ -29,13 +17,16 @@ const Banner = () => {
   };
 
   return (
-    <Container maxWidth={false} className={clsx(classes.container)}>
-      <Slider {...settings}>
-        <Image src={img1} />
-        <Image src={img2} />
-        <Image src={img3} />
-      </Slider>
-    </Container>
+    <Carousel
+      isBanner
+      slider={
+        <Slider {...settings}>
+          <Image src={img1} />
+          <Image src={img2} />
+          <Image src={img3} />
+        </Slider>
+      }
+    />
   );
 };
 
