7ad02462caf03e1fc0b3067fd933597647dc03c1
Shakiba448 <shakiba448@gmail.com>
2021-08-02 11:34:59 +0600

    WIP on Header

--- client/src/Re_components/Cards.js
+++ client/src/Re_components/Cards.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import { CardActionArea, CardContent, CardMedia } from "@material-ui/core";
+import { CardActionArea, CardContent, Box } from "@material-ui/core";
 import clsx from "clsx";
 import Text from "./Text";
 import Image from "next/image";
@@ -9,38 +8,42 @@ import Image from "next/image";
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
+    margin: "20px 20px",
+    background: "transparent",
   },
   media: {
-    height: 140,
     position: "relative",
+    borderRadius: "5px",
   },
   cardContent: {
     position: "absolute",
     top: 10,
     right: 10,
+    padding: "5px 15px",
+    backgroundColor: "red",
   },
 });
 
-const Cards = ({ col }) => {
+const Cards = ({ title, image, height = 345, width = 345 }) => {
   const classes = useStyles();
 
   return (
-    <Card className={clsx(classes.root)}>
+    <Box component="div" className={clsx(classes.root)}>
       <CardActionArea>
         <Image
           className={clsx(classes.media)}
           src="/images/c1.png"
           alt="images"
-          height={345}
-          width={345}
+          height={height}
+          width={width}
         />
         <CardContent className={clsx(classes.cardContent)}>
           <Text gutterBottom variant="h5" component="h2">
-            Lizard
+            {title}
           </Text>
         </CardContent>
       </CardActionArea>
-    </Card>
+    </Box>
   );
 };
 
