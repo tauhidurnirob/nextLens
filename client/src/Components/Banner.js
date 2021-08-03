56283b59f697dd5d17e508157eb2f60e33945000
Shakiba448 <shakiba448@gmail.com>
2021-08-03 14:06:19 +0600

    WIP on card

--- client/src/Components/Banner.js
+++ client/src/Components/Banner.js
@@ -1,9 +1,24 @@
 import React, { Fragment } from "react";
 import Image from "next/image";
+import { Grid, makeStyles } from "@material-ui/core";
+import clsx from "clsx";
 
 import { Carousel } from "../Re_components";
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    maxWidth: "1440px",
+    width: "100%",
+    padding: "0 10px",
+    margin: "20px auto 0 auto",
+  },
+
+  virtual: { padding: `${theme.spacing(8)}px 0 10px 0`, height: "474px" },
+}));
+
 const Banner = () => {
+  const classes = useStyles();
+
   const settings = {
     infinite: true,
     fade: true,
@@ -14,15 +29,27 @@ const Banner = () => {
   };
 
   return (
-    <Carousel
-      isBanner
-      {...settings}
-      slider={images.map((item, index) => (
-        <Fragment key={index}>
-          <Image src={item.image} alt={item.image} height={400} width={400} />
-        </Fragment>
-      ))}
-    />
+    <Grid container direction="row" className={clsx(classes.container)}>
+      <Grid item container md={4} lg={4} className={clsx(classes.virtual)}>
+        <Image src="/images/virtual.png" height={200} width={400} />
+      </Grid>
+      <Grid item container md={8} lg={8}>
+        <Carousel
+          isBanner
+          {...settings}
+          slider={images.map((item, index) => (
+            <Fragment key={index}>
+              <Image
+                src={item.image}
+                alt={item.image}
+                height={400}
+                width={400}
+              />
+            </Fragment>
+          ))}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
--- client/src/Re_components/Cards.js
+++ client/src/Re_components/Cards.js
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import { CardContent, Box, IconButton } from "@material-ui/core";
 import clsx from "clsx";
 import Image from "next/image";
-import colors from "../../config/colors";
 import VisibilityOutlinedIcon from "@material-ui/icons/VisibilityOutlined";
 import ShoppingBasketOutlinedIcon from "@material-ui/icons/ShoppingBasketOutlined";
 import Link from "next/link";
