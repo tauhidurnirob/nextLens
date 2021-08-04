6b5a2191d5527fef4312aca3ee0dc0a448550dbd
Shakiba448 <shakiba448@gmail.com>
2021-08-04 11:40:39 +0600

    WIP on Offers

--- client/src/Re_components/Cards.js
+++ client/src/Re_components/Cards.js
@@ -9,6 +9,7 @@ import Link from "next/link";
 import styles from "../../styles/imageHover.module.scss";
 import Button from "./AppButton";
 import Text from "./Text";
+import colors from "../../config/colors";
 
 const useStyles = makeStyles({
   root: {
@@ -24,11 +25,40 @@ const useStyles = makeStyles({
     padding: "5px 15px",
     borderRadius: "5px",
   },
-  productContent: {
+  productView: {
     position: "absolute",
     top: 5,
     left: 5,
     zIndex: 1,
+  },
+  productAddToCart: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    zIndex: 1,
+  },
+  btn: {
+    transition: "all 300ms ease-in-out",
+    boxShadow: "1px 1px 0 0 rgb(0 0 0 / 10%)",
+    padding: "10px 20px",
+    background: colors.white,
+    letterSpacing: "2px",
+    maxWidth: "200px",
+    borderRadius: 0,
+    borderColor: "2px solid #222",
+    fontSize: "14px",
+    fontWeight: "bold",
+    borderRadius: "60px",
+    "&:hover": {
+      boxShadow: "1px 1px 0 0 rgb(0 0 0 / 10%)",
+      background: colors.black,
+      color: colors.white,
+    },
+    "&:focus": {
+      background: colors.black,
+      color: colors.white,
+    },
   },
 });
 
@@ -45,23 +75,64 @@ const Cards = ({
 
   return (
     <Box component="div" className={clsx(classes.root)}>
-      <Box
-        component="div"
-        className={clsx({
-          [styles.img]: isCategory,
-          [styles.imgHover]: isCategory,
-          [styles.imgHover]: isHover,
-        })}
-      >
-        <Image
-          blurDataURL
-          src={item.image}
-          // loader={() => "/images/imageLoader.svg"}
-          alt={item.title}
-          height={height}
-          width={width}
-        />
-      </Box>
+      {isProduct ? (
+        <Box component="div" className={clsx(styles.productArea)}>
+          <Box component="div" className={clsx(styles.productOverlay)} />
+          <Image
+            blurDataURL
+            src={item.image}
+            // loader={() => "/images/imageLoader.svg"}
+            alt={item.title}
+            height={height}
+            width={width}
+          />
+
+          <Box
+            component="div"
+            className={clsx({
+              [classes.productView]: isProduct,
+            })}
+          >
+            <Link href={`/product-details/${item.id}`}>
+              <a>
+                <IconButton size="medium" color="primary">
+                  <VisibilityOutlinedIcon style={{ fontSize: 25 }} />
+                </IconButton>
+              </a>
+            </Link>
+          </Box>
+          <Box
+            component="div"
+            className={clsx({
+              [classes.productAddToCart]: isProduct,
+            })}
+          >
+            <Link href={`/product-details/${item.id}`}>
+              <a>
+                <Button className={clsx(classes.btn)}>Add To Cart</Button>
+              </a>
+            </Link>
+          </Box>
+        </Box>
+      ) : (
+        <Box
+          component="div"
+          className={clsx({
+            [styles.img]: isCategory,
+            [styles.imgHover]: isCategory,
+            [styles.imgHover]: isHover,
+          })}
+        >
+          <Image
+            blurDataURL
+            src={item.image}
+            // loader={() => "/images/imageLoader.svg"}
+            alt={item.title}
+            height={height}
+            width={width}
+          />
+        </Box>
+      )}
 
       {isCategory && (
         <CardContent
@@ -74,25 +145,6 @@ const Cards = ({
       )}
       {isProduct && (
         <>
-          <Box component="div" className={clsx(styles.productArea)}>
-            <Box component="div" className={clsx(styles.productOverlay)} />
-            <Image src="/images/kids.png" height={500} width={500} />
-
-            <Box
-              component="div"
-              className={clsx({
-                [classes.productContent]: isProduct,
-              })}
-            >
-              <Link href={`/product-details/${item.id}`}>
-                <a>
-                  <IconButton size="medium" color="primary">
-                    <VisibilityOutlinedIcon style={{ fontSize: 25 }} />
-                  </IconButton>
-                </a>
-              </Link>
-            </Box>
-          </Box>
           <Grid container direction="column">
             <Text style={{ fontWeight: "bold" }} color="secondary">
               shakib
