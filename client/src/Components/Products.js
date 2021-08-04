0bd2dc7ead8d7d490ec46f39e36fc94762b4a138
Shakiba448 <shakiba448@gmail.com>
2021-08-04 12:38:22 +0600

    WIP on Offers

--- client/src/Components/Products.js
+++ client/src/Components/Products.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, makeStyles } from "@material-ui/core";
+import clsx from "clsx";
 
 import { Cards } from "./../Re_components";
 import products from "../../fakeData/products";
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: `${theme.spacing(4)}px 0 10px 0`,
+  },
+}));
+
 const Products = () => {
+  const classes = useStyles();
   return (
-    <Grid container direction="row">
+    <Grid container direction="row" className={clsx(classes.container)}>
       {products.map((item) => (
         <Grid item key={item.id} container md={4} justifyContent="center">
           <Cards item={item} isProduct width={500} height={500} />
