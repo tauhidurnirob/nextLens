1e246b68aa3bad8b17fce43db53ff690d60edc24
Shakiba448 <shakiba448@gmail.com>
2021-08-05 12:23:04 +0600

    WIP on product details

--- client/pages/product-details/ProductContents.js
+++ client/pages/product-details/ProductContents.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Container, makeStyles, Grid, Box } from "@material-ui/core";
 import clsx from "clsx";
-import Text from "./../../src/Re_components/Text";
+
+import { Text } from "./../../src/Re_components";
+import colors from "../../config/colors";
 
 const useStyles = makeStyles((theme) => ({
-  container: {},
+  font: {
+    color: colors.black,
+    cursor: "pointer",
+    transition: "0.3s",
+    fontWeight: "500",
+    letterSpacing: "1px",
+    fontSize: "16px",
+    padding: "4px 0",
+    "&:hover": {
+      color: colors.sky,
+    },
+  },
 }));
 
 const ProductContents = ({ data }) => {
@@ -13,11 +26,14 @@ const ProductContents = ({ data }) => {
   return (
     <Grid item container md={8}>
       <Grid container direction="column">
-        <Text gutterBottom>
+        <Text gutterBottom variant="h6">
           <Box fontWeight="fontWeightBold">{data.title}</Box>
         </Text>
-        <Text color="textSecondary">
-          <Box fontWeight={500}>Be the first to review this product</Box>
+        <Text gutterBottom color="textSecondary" className={clsx(classes.font)}>
+          Be the first to review this product
+        </Text>
+        <Text gutterBottom color="textSecondary">
+          As low as ৳{data.price}
         </Text>
       </Grid>
     </Grid>
--- client/src/Components/EyeCom/EconomyEyeGlasses.js
+++ client/src/Components/EyeCom/EconomyEyeGlasses.js
@@ -15,7 +15,7 @@ const EconomyEyeGlasses = () => {
   return (
     <>
       <Heading className={clsx(classes.heading)} isDivider>
-        EYEGLASSES EYEGLASSES
+        EYEGLASSES SHAPE
       </Heading>
       <Products />
     </>
