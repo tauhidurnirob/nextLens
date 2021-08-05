cf08858d78f5c9c9977e1959527ea44df085efac
Shakiba448 <shakiba448@gmail.com>
2021-08-05 11:53:32 +0600

    WIP on support

---
+++ client/pages/product-details/ProductImage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Image from "next/image";
+
+const ProductImage = ({ data }) => {
+  return (
+    <Grid container direction="row">
+      <Grid item container md={6} justifyContent="center">
+        <Image src={data.image} alt={data.title} width={500} height={500} />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ProductImage;
