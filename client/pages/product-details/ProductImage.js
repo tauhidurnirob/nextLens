ee97889c65537ef274a40d7d92d031f86f848b67
Shakiba448 <shakiba448@gmail.com>
2021-08-07 19:43:10 +0600

    WIP on product details

--- client/pages/product-details/ProductImage.js
+++ client/pages/product-details/ProductImage.js
@@ -14,7 +14,7 @@ const ProductImage = ({ data }) => {
   };
   return (
     <Grid item container md={5}>
-      {data.detailsImage?.length === 0 ? (
+      {data?.detailsImage.length === 0 ? (
         <Image src={data?.image} alt={data.title} width={500} height={500} />
       ) : (
         <Carousel
