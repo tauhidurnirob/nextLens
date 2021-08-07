6d3f7eeaca87c3920141bae5f5ed572fe190f70f
Shakiba448 <shakiba448@gmail.com>
2021-08-07 15:31:54 +0600

    WIP on product details

--- client/src/Components/EyeCom/EyeglassesShape.js
+++ client/src/Components/EyeCom/EyeglassesShape.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
 import clsx from "clsx";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 
 import { Carousel, Heading, Cards } from "../../Re_components";
 import eyeShape from "../../../fakeData/eyeShape";
@@ -20,9 +18,6 @@ const EconomyEyeGlasses = () => {
     slidesToShow: 4,
     slidesToScroll: 4,
     autoplay: true,
-    arrows: true,
-    nextArrow: <ArrowBackIcon />,
-    prevArrow: <ArrowForwardIcon />,
 
     responsive: [
       {
@@ -57,6 +52,7 @@ const EconomyEyeGlasses = () => {
       <Heading className={clsx(classes.heading)} isDivider>
         EYEGLASSES SHAPE
       </Heading>
+
       <Carousel
         {...settings}
         slider={eyeShape.map((item) => (
--- client/src/Re_components/Carousel.js
+++ client/src/Re_components/Carousel.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Container, makeStyles, Grid } from "@material-ui/core";
+import { Container, makeStyles } from "@material-ui/core";
 import clsx from "clsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+
 import styles from "../../styles/image.module.scss";
 
 const useStyles = makeStyles((theme) => ({
