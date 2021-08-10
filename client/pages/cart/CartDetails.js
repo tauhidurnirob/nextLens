2828d66e9de2a5692b3e43aa20760434f1d43f57
Shakiba448 <shakiba448@gmail.com>
2021-08-10 15:05:03 +0600

    WIP on Cart

--- client/pages/cart/CartDetails.js
+++ client/pages/cart/CartDetails.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { Container, makeStyles } from "@material-ui/core";
 import clsx from "clsx";
+import { useSelector } from "react-redux";
 
 import { Heading, Text } from "./../../src/Re_components";
+import { cartList } from "./../../src/redux/slices/productSlice";
+import CartTable from "./CartTable";
 
 const useStyles = makeStyles((theme) => ({
   container: { padding: "84px 0 0 0 " },
 }));
 
-const ModelDetails = ({}) => {
+const CartDetails = ({}) => {
   const classes = useStyles();
+
+  const { cart } = useSelector(cartList);
 
   return (
     <Container maxWidth="lg" className={clsx(classes.container)}>
       <Heading className={clsx(classes.heading)} isDivider>
         <Text variant="h4">Cart</Text>
       </Heading>
+      <CartTable cartProduct={cart} />
     </Container>
   );
 };
 
-export default ModelDetails;
+export default CartDetails;
---
+++ client/pages/cart/CartTable.js
@@ -0,0 +1,93 @@
+import React from "react";
+import {
+  makeStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Box,
+} from "@material-ui/core";
+import Image from "next/image";
+import colors from "../../config/colors";
+
+import clsx from "clsx";
+import { AddToCartButton } from "../../src/Re_components";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  tableContainer: { marginTop: "24px" },
+  removeBtn: {
+    transition: "all 300ms ease-in-out",
+    boxShadow: "1px 1px 0 0 rgb(0 0 0 / 10%)",
+    padding: "10px 25px",
+    background: colors.black,
+    color: colors.white,
+    fontSize: "16px",
+    fontWeight: "bold",
+    marginLeft: "20px",
+    borderRadius: "50px",
+    "&:hover": {
+      boxShadow: "1px 1px 0 0 rgb(0 0 0 / 10%)",
+      background: colors.sky,
+      color: colors.white,
+    },
+  },
+});
+
+const CartTable = ({ cartProduct }) => {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper} className={clsx(classes.tableContainer)}>
+      <Table className={clsx(classes.table)} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell align="center"></TableCell>
+            <TableCell align="center">Product</TableCell>
+            <TableCell align="center">Price</TableCell>
+            <TableCell align="center">Quantity</TableCell>
+            <TableCell align="center">Total</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {cartProduct.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell component="th" scope="row">
+                <AddToCartButton
+                  item={item}
+                  className={clsx(classes.removeBtn)}
+                />
+              </TableCell>
+              <TableCell align="center">
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  height={100}
+                  width={100}
+                />
+              </TableCell>
+              <TableCell align="center">{item.title}</TableCell>
+              <TableCell align="center">
+                <Box fontWeight="fontWeightBold">৳{item.price}</Box>
+              </TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center">
+                <Box style={{ color: colors.sky }} fontWeight="fontWeightBold">
+                  ৳{item.price}
+                </Box>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default CartTable;
