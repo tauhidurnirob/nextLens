78016cab48262268e0c21de20d7fd050e516167f
Shakiba448 <shakiba448@gmail.com>
2021-08-12 16:44:51 +0600

    WIP on Sidebar

--- client/pages/product-details/[id].js
+++ client/pages/product-details/[id].js
@@ -7,7 +7,8 @@ import { findById } from "../../src/redux/slices/productSlice";
 
 const ProductId = ({ data }) => {
   const dispatch = useDispatch();
-  const mainData = { ...data, quantity: 2, totalPrice: data.price };
+
+  const mainData = { ...data, quantity: 3, totalPrice: data.price };
 
   dispatch(findById(mainData));
 
