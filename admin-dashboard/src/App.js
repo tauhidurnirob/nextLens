3a04f094112148c216b158022dcc9101aa245ca4
Shakiba448 <shakiba448@gmail.com>
2021-08-15 14:49:51 +0600

    WIP on Admin-Dashbaord

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,7 +1,18 @@
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import GlobalStyles from "./config/GlobalStyles";
+import theme from "./config/theme";
 import Dashboard from "./Components/Dashboard";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles />
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ admin-dashboard/src/config/colors.js
@@ -0,0 +1,8 @@
+export default {
+  white: "#fff",
+  black: "#000",
+  offWhite: "#FAF9F6",
+  sky: "#56cfe1",
+  grey: "#F6F6F8",
+  font: "#CDC6B6",
+};
