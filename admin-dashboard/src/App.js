f759211fa2cbaf9af5d3071119f009f9498211a9
Shakiba448 <shakiba448@gmail.com>
2021-08-19 19:02:42 +0600

    WIP on Admin-Dashboard

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -4,12 +4,17 @@ import { Routes, Route } from "react-router-dom";
 
 import GlobalStyles from "./config/GlobalStyles";
 import theme from "./config/theme";
+import Layout from "./Components/Layout";
 
 import routes from "./routes";
 
 function App() {
-  const routeComponents = routes.map(({ path, component }, index) => (
-    <Route key={index} path={path} element={component} />
+  const routeComponents = routes.map(({ path, title, component }, index) => (
+    <Route
+      key={index}
+      path={path}
+      element={<Layout title={title}>{component}</Layout>}
+    />
   ));
 
   return (
--- admin-dashboard/src/routes.js
+++ admin-dashboard/src/routes.js
@@ -1,41 +1,20 @@
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Product from "./Components/Products/Product";
 import AddProductForm from "./Components/Products/AddProductForm";
-import Layout from "./Components/Layout";
 
 const routes = [
-  {
-    path: "/",
-    component: (
-      <Layout title="Dashboard">
-        <Dashboard />
-      </Layout>
-    ),
-  },
-  {
-    path: "/product",
-    component: (
-      <Layout title="Product">
-        <Product />
-      </Layout>
-    ),
-  },
+  { path: "/", component: <Dashboard />, title: "Dashboard" },
+  { path: "/product", component: <Product />, title: "Product" },
   {
     path: "/product/add-product",
-    component: (
-      <Layout title="Add Product">
-        <AddProductForm />
-      </Layout>
-    ),
+    component: <AddProductForm />,
+    title: "Add Product",
   },
-  { path: "/category", component: <Layout title="Category">Category</Layout> },
-  { path: "/orders", component: <Layout title="Orders">Orders</Layout> },
-  {
-    path: "/customers",
-    component: <Layout title="Customers">Customers</Layout>,
-  },
-  { path: "/coupons", component: <Layout title="Coupons">Coupons</Layout> },
-  { path: "/settings", component: <Layout title="Settings">Settings</Layout> },
+  { path: "/category", component: "Category", title: "Category" },
+  { path: "/orders", component: "Orders", title: "Orders" },
+  { path: "/customers", component: "Customers", title: "Customers" },
+  { path: "/coupons", component: "Coupons", title: "Coupons" },
+  { path: "/settings", component: "Settings", title: "Settings" },
 ];
 
 export default routes;
