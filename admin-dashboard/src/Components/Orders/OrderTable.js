6d980a153458076e48f4c45ba2529d39169086df
Shakiba448 <shakiba448@gmail.com>
2021-08-21 16:28:07 +0600

    TS Admin Orders

--- admin-dashboard/src/Components/Orders/OrderTable.js
+++ admin-dashboard/src/Components/Orders/OrderTable.js
@@ -1,129 +1,208 @@
 import React from "react";
 import {
   makeStyles,
+  Paper,
+  Box,
+  Collapse,
+  IconButton,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
-  Paper,
   Typography,
-  Box,
 } from "@material-ui/core";
+
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import clsx from "clsx";
+
+const useRowStyles = makeStyles({
+  root: {
+    "& > *": {
+      borderBottom: "unset",
+    },
+  },
+});
+
+function createData(id, deliveryAddress, paymentMethod, status) {
+  return {
+    id,
+    deliveryAddress,
+    paymentMethod,
+    status,
+    history: [
+      {
+        date: "2020-01-05",
+        customerId: "11091700",
+        contact: "715-371-3507",
+        price: 399,
+      },
+      {
+        date: "2020-01-02",
+        customerId: "11091700",
+        contact: "715-371-3507",
+        price: 399,
+      },
+    ],
+  };
+}
+
+const Row = ({ row }) => {
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
+
+  return (
+    <>
+      <TableRow className={clsx(classes.root)}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.id}
+        </TableCell>
+        <TableCell align="center">{row.deliveryAddress}</TableCell>
+        <TableCell align="center">{row.paymentMethod}</TableCell>
+        <TableCell align="center">{row.status}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="h6" gutterBottom component="div">
+                History
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Customer</TableCell>
+                    <TableCell align="center">Contact</TableCell>
+                    <TableCell align="center">Total price ($)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.date}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.date}
+                      </TableCell>
+                      <TableCell>{historyRow.customerId}</TableCell>
+                      <TableCell align="center">{historyRow.contact}</TableCell>
+                      <TableCell align="center">${historyRow.price}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+};
+
+const rows = [
+  createData(
+    14,
+    "29 Eve Street, 543 Evenue Road, Ny 87876",
+    "Cash On Delivery",
+    "Processing"
+  ),
+  createData(
+    14,
+    "29 Eve Street, 543 Evenue Road, Ny 87876",
+    "Cash On Delivery",
+    "Processing"
+  ),
+  createData(
+    14,
+    "29 Eve Street, 543 Evenue Road, Ny 87876",
+    "Cash On Delivery",
+    "Processing"
+  ),
+
+  createData(
+    14,
+    "29 Eve Street, 543 Evenue Road, Ny 87876",
+    "Cash On Delivery",
+    "Processing"
+  ),
+  createData(
+    14,
+    "29 Eve Street, 543 Evenue Road, Ny 87876",
+    "Cash On Delivery",
+    "Processing"
+  ),
+  createData(
+    14,
+    "29 Eve Street, 543 Evenue Road, Ny 87876",
+    "Cash On Delivery",
+    "Processing"
+  ),
+  createData(
+    14,
+    "29 Eve Street, 543 Evenue Road, Ny 87876",
+    "Cash On Delivery",
+    "Processing"
+  ),
+];
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
-  },
-  imageList: {
-    width: 70,
-    height: 70,
+    minWidth: 1024,
   },
   tableContainer: {
     height: "500px",
   },
 });
 
-function createData(id, image, name, slug, type) {
-  return { id, image, name, slug, type };
-}
-
-const rows = [
-  createData(
-    15,
-    "https://i.ibb.co/zm0ZmFW/alexa.jpg",
-    "Frozen yoghurt",
-    "meat-and-fish",
-    "bag"
-  ),
-  createData(
-    15,
-    "https://i.ibb.co/zm0ZmFW/alexa.jpg",
-    "Frozen yoghurt",
-    "meat-and-fish",
-    "bag"
-  ),
-  createData(
-    15,
-    "https://i.ibb.co/zm0ZmFW/alexa.jpg",
-    "Frozen yoghurt",
-    "meat-and-fish",
-    "bag"
-  ),
-  createData(
-    15,
-    "https://i.ibb.co/zm0ZmFW/alexa.jpg",
-    "Frozen yoghurt",
-    "meat-and-fish",
-    "bag"
-  ),
-  createData(
-    15,
-    "https://i.ibb.co/zm0ZmFW/alexa.jpg",
-    "Frozen yoghurt",
-    "meat-and-fish",
-    "bag"
-  ),
-];
-
 const OrderTable = () => {
   const classes = useStyles();
 
   return (
-    <TableContainer className={clsx(classes.tableContainer)} component={Paper}>
-      <Table
-        stickyHeader
-        aria-label="sticky table"
-        className={clsx(classes.table)}
-      >
+    <TableContainer
+      stickyHeader
+      aria-label="sticky table"
+      className={clsx(classes.tableContainer)}
+      component={Paper}
+    >
+      <Table className={clsx(classes.table)} aria-label="collapsible table">
         <TableHead>
           <TableRow>
+            <TableCell />
             <TableCell>
-              <Typography variant="subtitle1">
-                <Box fontWeight="fontWeightBold">Id</Box>
+              <Typography variant="subtitle2">
+                <Box fontWeight="fontWeightBold">ID</Box>
               </Typography>
             </TableCell>
             <TableCell align="center">
-              <Typography variant="subtitle1">
-                <Box fontWeight="fontWeightBold">Image</Box>
+              <Typography variant="subtitle2">
+                <Box fontWeight="fontWeightBold">Delivery Address</Box>
               </Typography>
             </TableCell>
             <TableCell align="center">
-              <Typography variant="subtitle1">
-                <Box fontWeight="fontWeightBold">Name</Box>
+              <Typography variant="subtitle2">
+                <Box fontWeight="fontWeightBold">Payment Method</Box>
               </Typography>
             </TableCell>
             <TableCell align="center">
-              <Typography variant="subtitle1">
-                <Box fontWeight="fontWeightBold">Slug</Box>
-              </Typography>
-            </TableCell>
-            <TableCell align="center">
-              <Typography variant="subtitle1">
-                <Box fontWeight="fontWeightBold">Type</Box>
+              <Typography variant="subtitle2">
+                <Box fontWeight="fontWeightBold">Status</Box>
               </Typography>
             </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="center">
-                <img
-                  src={row.image}
-                  alt={row.name}
-                  className={clsx(classes.imageList)}
-                />
-              </TableCell>
-              <TableCell align="center">{row.name}</TableCell>
-              <TableCell align="center">{row.slug}</TableCell>
-              <TableCell align="center">{row.type}</TableCell>
-            </TableRow>
+            <Row key={row.name} row={row} />
           ))}
         </TableBody>
       </Table>
