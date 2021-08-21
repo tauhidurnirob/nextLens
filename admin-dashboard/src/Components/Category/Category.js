f06f7a6701e437acd97060d85fb6352fdff322cf
Shakiba448 <shakiba448@gmail.com>
2021-08-21 10:07:35 +0600

    TS Crash Course

---
+++ admin-dashboard/src/Components/Category/Category.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container, Grid, Box } from "@material-ui/core";
+import ProductForm from "./ProductForm";
+import products from "../../fakeData/products";
+import { Cards } from "../../Re_components";
+
+const Category = () => {
+  document.title = "Category";
+
+  return (
+    <Container maxWidth="lg">
+      <ProductForm />
+      <Box mt={4} mb={4}>
+        <Grid container direction="row" spacing={4} justifyContent="center">
+          {products.map((item, index) => (
+            <Grid key={index} item container md={4}>
+              <Cards items={item} isTruncate line={1} descLine={4} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
+
+export default Category;
