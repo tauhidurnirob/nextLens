96f5b3018bcf3109ed665f8b50cfa5e2ac44b936
Shakiba448 <shakiba448@gmail.com>
2021-08-23 12:12:36 +0600

    WIP on Server

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -9,7 +9,7 @@ import generateToken from "../utils/generateToken.js";
 export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email: email, password: password });
+  const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
     res.json({
@@ -30,32 +30,33 @@ export const authUser = asyncHandler(async (req, res) => {
 // @access  Public
 
 export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
   console.log(req.body);
-  //   const { name, email, password } = req.body;
 
-  //   const userExists = await User.find({ email });
+  const userExists = await User.findOne({ email });
 
-  //   if (userExists) {
-  //     res.status(400);
-  //     throw new Error("User already exists");
-  //   }
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-  //   const user = await User.create({
-  //     name,
-  //     email,
-  //     password,
-  //   });
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
 
-  //   if (user) {
-  //     res.status(201).json({
-  //       _id: user._id,
-  //       name: user.name,
-  //       email: user.email,
-  //       isAdmin: user.isAdmin,
-  //       token: generateToken(user._id),
-  //     });
-  //   } else {
-  //     res.status(400);
-  //     throw new Error("Invalid user data");
-  //   }
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
