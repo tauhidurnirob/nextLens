9e349ac69aad2a2a6e62c69389db5d4c0207676c
Shakiba448 <shakiba448@gmail.com>
2021-08-23 12:15:17 +0600

    WIP on Server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import "colors";
 import morgan from "morgan";
+import bodyParser from "body-parser";
 
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
@@ -12,44 +13,26 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// if (process.env.NODE_ENV === "development") {
+// configs
 app.use(morgan("dev"));
-// }
+dotenv.config();
+connectDB();
 
 // Body parser
 
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 
-dotenv.config();
+// apis
+app.use("/api/users", userRoutes);
 
-connectDB();
+// middleware
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
-// app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
-// app.use("/api/orders", orderRoutes);
-// app.use("/api/upload", uploadRoutes);
-
-// app.get("/api/config/paypal", (req, res) =>
-//   res.send(process.env.PAYPAL_CLIENT_ID)
-// );
-
-// File
-// const __dirname = path.resolve();
-// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
-
-// Not found api
-app.use(notFound);
-// errorHandler
-app.use(errorHandler);
 
 app.listen(
   PORT,
