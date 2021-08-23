5d5e957b85408534d31fef4f0f1d2dd3ebe6e68d
Shakiba448 <shakiba448@gmail.com>
2021-08-23 09:35:59 +0600

    WIP on Server

---
+++ server/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import "colors";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import morgan from "morgan";
+import cors from "cors";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// if (process.env.NODE_ENV === "development") {
+app.use(morgan("dev"));
+// }
+
+// Body parser
+
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// app.use("/api/products", productRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/orders", orderRoutes);
+// app.use("/api/upload", uploadRoutes);
+
+// app.get("/api/config/paypal", (req, res) =>
+//   res.send(process.env.PAYPAL_CLIENT_ID)
+// );
+
+// File
+// const __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
+// Not found api
+app.use(notFound);
+// errorHandler
+app.use(errorHandler);
+
+app.listen(
+  PORT,
+  console.log(`Server is running on http://localhost:${PORT}`.yellow.bold)
+);
