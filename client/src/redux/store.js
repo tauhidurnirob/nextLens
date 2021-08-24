6da0028bbdadf3b82f5740f21ea0a3f8758c6f95
Shakiba448 <shakiba448@gmail.com>
2021-08-24 09:15:00 +0600

    WIP on Server

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 
-const userInfoFromStorage = localStorage.getItem("userInfo")
-  ? JSON.parse(localStorage.getItem("userInfo"))
-  : [];
-
 const store = configureStore({
   reducer,
 });
