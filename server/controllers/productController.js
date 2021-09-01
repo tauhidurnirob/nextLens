afee33277941f19ee2c35458b5ec88bc50276133
Shakiba448 <shakiba448@gmail.com>
2021-09-01 11:02:36 +0600

    WIP on Server

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -95,13 +95,12 @@ export const createProduct = asyncHandler(async (req, res) => {
 // @access public
 
 export const getProductByCategory = asyncHandler(async (req, res) => {
-  console.log(req.params.id);
-  // const product = await Product.find({ slug: req.params.id });
+  const product = await Product.find({}).where({ category: req.params.id });
 
-  // if (product) {
-  //   res.json(product);
-  // } else {
-  //   res.status(404);
-  //   throw new Error("Product not found");
-  // }
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
 });
