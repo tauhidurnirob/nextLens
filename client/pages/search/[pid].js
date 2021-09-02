25e1fbd33ea4d694b55f70bd22af7e64c19f4b32
Shakiba448 <shakiba448@gmail.com>
2021-09-02 12:38:36 +0600

    WIP on Server

--- client/pages/search/[pid].js
+++ client/pages/search/[pid].js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { Container, Box } from "@material-ui/core";
 
+import Products from "../../src/Components/Products";
+import { fetchedProducts } from "../../src/redux/slices/productSlice";
 import productApi from "../api/products";
+import Layout from "./../../src/Re_components/Layout";
 
 const Search = ({ data }) => {
-  return <h1>Post:</h1>;
+  const dispatch = useDispatch();
+
+  dispatch(fetchedProducts(data.products));
+  return (
+    <Layout title="Search">
+      <Container maxWidth="lg">
+        <Box mt={4}>
+          <Products />
+        </Box>
+      </Container>
+    </Layout>
+  );
 };
 
 export default Search;
