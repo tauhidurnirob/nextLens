c79d31a20405b3313b9c6100bd855e27c4f29b0a
Shakiba448 <shakiba448@gmail.com>
2021-09-02 12:47:55 +0600

    WIP on Server

--- client/src/Components/Products.js
+++ client/src/Components/Products.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid, makeStyles, Container, Button } from "@material-ui/core";
 import clsx from "clsx";
 import { useSelector } from "react-redux";
+import Image from "next/image";
+import HistoryIcon from "@material-ui/icons/History";
+import { useRouter } from "next/router";
 
 import { Cards } from "./../Re_components";
 import { cartList } from "./../redux/slices/productSlice";
@@ -14,15 +17,38 @@ const useStyles = makeStyles((theme) => ({
 
 const Products = () => {
   const { products } = useSelector(cartList);
-
+  const { back } = useRouter();
   const classes = useStyles();
   return (
     <Grid container direction="row" className={clsx(classes.container)}>
-      {products.map((item) => (
-        <Grid item key={item.id} container md={4} justifyContent="center">
-          <Cards item={item} isProduct width={500} height={500} />
-        </Grid>
-      ))}
+      {products.length !== 0 ? (
+        products.map((item) => (
+          <Grid item key={item.id} container md={4} justifyContent="center">
+            <Cards item={item} isProduct width={500} height={500} />
+          </Grid>
+        ))
+      ) : (
+        <Container maxWidth="lg">
+          <Grid container justifyContent="center">
+            <Image
+              src="/images/oops.png"
+              alt="No product found"
+              width={500}
+              height={500}
+            />
+          </Grid>
+          <Grid container justifyContent="center">
+            <Button
+              startIcon={<HistoryIcon />}
+              variant="outlined"
+              color="primary"
+              onClick={() => back()}
+            >
+              Go Back
+            </Button>
+          </Grid>
+        </Container>
+      )}
     </Grid>
   );
 };
