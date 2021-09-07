725003da4729dc06ae6e2d397fcd3ff8777757f2
Shakiba448 <shakiba448@gmail.com>
2021-09-07 14:34:01 +0600

    WIP on Payment

--- client/pages/payment/index.js
+++ client/pages/payment/index.js
@@ -5,12 +5,12 @@ import {
   Grid,
   FormControl,
   FormGroup,
-  FormLabel,
   FormControlLabel,
   Checkbox,
 } from "@material-ui/core";
 import clsx from "clsx";
 import CheckoutSteps from "./../../src/Re_components/CheckoutSteps";
+import { Layout } from "./../../src/Re_components";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -33,45 +33,47 @@ const index = () => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
   return (
-    <Container className={clsx(classes.container)}>
-      <CheckoutSteps step1 step2 step3 />
-      <Grid container justifyContent="center">
-        <FormControl component="fieldset" className={classes.formControl}>
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={paypal}
-                  onChange={handleChange}
-                  name="paypal"
-                />
-              }
-              label="PAYPAL"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={stripe}
-                  onChange={handleChange}
-                  name="stripe"
-                />
-              }
-              label="STRIPE"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={cashOn}
-                  onChange={handleChange}
-                  name="cashOn"
-                />
-              }
-              label="CASH ON"
-            />
-          </FormGroup>
-        </FormControl>
-      </Grid>
-    </Container>
+    <Layout title="Payment">
+      <Container className={clsx(classes.container)}>
+        <CheckoutSteps step1 step2 step3 />
+        <Grid container justifyContent="center">
+          <FormControl component="fieldset" className={classes.formControl}>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={paypal}
+                    onChange={handleChange}
+                    name="paypal"
+                  />
+                }
+                label="PAYPAL"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={stripe}
+                    onChange={handleChange}
+                    name="stripe"
+                  />
+                }
+                label="STRIPE"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={cashOn}
+                    onChange={handleChange}
+                    name="cashOn"
+                  />
+                }
+                label="CASH ON"
+              />
+            </FormGroup>
+          </FormControl>
+        </Grid>
+      </Container>
+    </Layout>
   );
 };
 
