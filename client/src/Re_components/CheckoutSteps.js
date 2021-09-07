a7f7471b86a928665bb01369612623890f7ac957
Shakiba448 <shakiba448@gmail.com>
2021-09-07 12:10:19 +0600

    WIP on Contact

--- client/src/Re_components/CheckoutSteps.js
+++ client/src/Re_components/CheckoutSteps.js
@@ -31,34 +31,67 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <Grid container direction="row" justifyContent="center">
         <Grid item>
           <ButtonGroup color="inherit">
-            <Box mr={2}>
-              <Link href="/register">
-                <Button className={clsx(classes.linkButtonHover)}>
-                  Sign In
+            {step1 ? (
+              <Box mr={2}>
+                <Link href="/register">
+                  <Button className={clsx(classes.linkButtonHover)}>
+                    Sign In
+                  </Button>
+                </Link>
+              </Box>
+            ) : (
+              <Box mr={2}>
+                <Button disabled className={clsx(classes.linkButtonHover)}>
+                  <strike>Sign In</strike>
                 </Button>
-              </Link>
-            </Box>
-            <Box mr={2}>
-              <Link href="/register">
-                <Button className={clsx(classes.linkButtonHover)}>
-                  Shipping Address
+              </Box>
+            )}
+
+            {step2 ? (
+              <Box mr={2}>
+                <Link href="/register">
+                  <Button className={clsx(classes.linkButtonHover)}>
+                    Shipping Address
+                  </Button>
+                </Link>
+              </Box>
+            ) : (
+              <Box mr={2}>
+                <Button disabled className={clsx(classes.linkButtonHover)}>
+                  <strike>Shipping Address</strike>
                 </Button>
-              </Link>
-            </Box>
-            <Box mr={2}>
-              <Link href="/register">
-                <Button className={clsx(classes.linkButtonHover)}>
-                  Payment
+              </Box>
+            )}
+            {step3 ? (
+              <Box mr={2}>
+                <Link href="/register">
+                  <Button className={clsx(classes.linkButtonHover)}>
+                    Payment
+                  </Button>
+                </Link>
+              </Box>
+            ) : (
+              <Box mr={2}>
+                <Button disabled className={clsx(classes.linkButtonHover)}>
+                  <strike>Payment</strike>
                 </Button>
-              </Link>
-            </Box>
-            <Box mr={2}>
-              <Link href="/register">
-                <Button className={clsx(classes.linkButtonHover)}>
-                  Place Order
+              </Box>
+            )}
+            {step4 ? (
+              <Box mr={2}>
+                <Link href="/register">
+                  <Button className={clsx(classes.linkButtonHover)}>
+                    Place Order
+                  </Button>
+                </Link>
+              </Box>
+            ) : (
+              <Box mr={2}>
+                <Button disabled className={clsx(classes.linkButtonHover)}>
+                  <strike>Place Order</strike>
                 </Button>
-              </Link>
-            </Box>
+              </Box>
+            )}
           </ButtonGroup>
         </Grid>
       </Grid>
