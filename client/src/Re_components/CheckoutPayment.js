a77ec61d999a84dfbf797de8abbc421dd9af039f
Shakiba448 <shakiba448@gmail.com>
2021-09-12 11:48:35 +0600

    WIP on NEXT LENS

--- client/src/Re_components/CheckoutPayment.js
+++ client/src/Re_components/CheckoutPayment.js
@@ -6,6 +6,7 @@ import {
   Typography,
   Divider,
   Box,
+  Avatar,
 } from "@material-ui/core";
 import clsx from "clsx";
 
@@ -29,8 +30,6 @@ const CheckoutPayment = ({ paymentMethod }) => {
             </Typography>
           </Grid>
           <Divider style={{ marginBottom: "20px", width: "90%" }} />
-        </Grid>
-        <Grid item container md={7}>
           <Grid container direction="column">
             <Typography variant="h4" gutterBottom color="textSecondary">
               PAYMENT METHOD
@@ -40,16 +39,39 @@ const CheckoutPayment = ({ paymentMethod }) => {
             </Typography>
           </Grid>
           <Divider style={{ marginBottom: "20px", width: "90%" }} />
-        </Grid>
-        <Grid item container md={7}>
           <Grid container direction="column">
             <Typography variant="h4" gutterBottom color="textSecondary">
               ORDER ITEMS
             </Typography>
-            <Container maxWidth="lg"></Container>
+            <Container maxWidth="lg">
+              <Grid container direction="column">
+                <Grid item container md={12}>
+                  <Grid
+                    item
+                    container
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                    style={{ width: "90%" }}
+                  >
+                    <Avatar variant="square">N</Avatar>
+                    <Typography variant="subtitle1" gutterBottom>
+                      <Box fontWeight="fontWeightBold">
+                        iPhone 11 Pro 256GB Memory
+                      </Box>
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                      <Box fontWeight="fontWeightBold">
+                        1 x $599.99 = $599.99
+                      </Box>
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Container>
           </Grid>
         </Grid>
-        <Grid container md={5}>
+        <Grid container md={5} justifyContent="center">
           <Typography variant="h4" gutterBottom>
             {paymentMethod}
           </Typography>
