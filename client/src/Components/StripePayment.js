77760ca55a4cf6569c66aff72f739febdd211838
Shakiba448 <shakiba448@gmail.com>
2021-09-15 13:33:00 +0600

    WIP on NEXT LENS

--- client/src/Components/StripePayment.js
+++ client/src/Components/StripePayment.js
@@ -1,24 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "@material-ui/core";
 import StripeCheckout from "react-stripe-checkout";
 
 import stripeApi from "../../pages/api/stripe";
 
 const StripePayment = () => {
-  const [product, setProduct] = useState({
-    name: "React form FB",
-    price: 100,
-    productBy: "Muktadir",
-  });
-
   const makePayment = async (token) => {
-    const body = { token, product };
+    const body = { token };
     const { data } = await stripeApi.createPaymentStripe(body);
     console.log(data);
   };
   return (
     <StripeCheckout
-      stripeKey="pk_test_51HZrZoASRJydd9konZNtAoNFNfQSJttzAyouQzbGmwuq0mTl6l2c9B4Bw2X0ZnB146zEd5WTLSlC1jfaFRS9X7Ic00MSR4xE3i"
+      stripeKey={process.env.NEXT_PUBLIC_PUBLISH_KEY}
       token={makePayment}
       name="Buy React"
       amount={200 * 100}
