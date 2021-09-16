e63ce3613e7ffa33afaff5ab20a9df1e993f4d44
Shakiba448 <shakiba448@gmail.com>
2021-09-16 16:13:24 +0600

    WIP on NEXT LENS

--- server/controllers/cashOnDeliveryController.js
+++ server/controllers/cashOnDeliveryController.js
@@ -10,6 +10,7 @@ dotenv.config();
 // @routes POST/api/config/cashOnDelivery
 // @access public
 const imagePath = path.resolve("img", "invoice.png");
+const draftPath = path.resolve("img", "draft.png");
 
 const base64_encode = (img) => {
   const png = fs.readFileSync(img);
@@ -28,7 +29,7 @@ export const createCashOnDelivery = asyncHandler(async (req, res) => {
     marginLeft: 25,
     marginBottom: 25,
     logo: base64_encode(imagePath),
-    background: "https://public.easyinvoice.cloud/img/watermark-draft.jpg",
+    background: base64_encode(draftPath),
     sender: {
       company: "NEXT-LENSE",
       address: "MIRPUR-1 DHAKA",
