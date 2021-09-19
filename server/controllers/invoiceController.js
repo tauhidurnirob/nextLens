cb7d48a22e24e85fe31db4120d8ab8b7a3e4a4f8
Shakiba448 <shakiba448@gmail.com>
2021-09-19 21:24:09 +0600

    WIP on NEXT LENS

--- server/controllers/invoiceController.js
+++ server/controllers/invoiceController.js
@@ -17,7 +17,7 @@ const base64_encode = (img) => {
 };
 
 export const createInvoice = asyncHandler(async (req, res) => {
-  const { title, name, email, shipping, address, cart } = req.body;
+  const { text, title, name, email, shipping, address, cart } = req.body;
   const today = new Date();
 
   const data = {
@@ -72,7 +72,7 @@ export const createInvoice = asyncHandler(async (req, res) => {
       from: process.env.FORM,
       to: email,
       subject: "INVOICE",
-      text: "Thanks for with us. We will give our best services as you expect from us. Thanks NEXT-LENS Team.",
+      text: text,
       attachments: [
         {
           filename: "invoice.pdf",
