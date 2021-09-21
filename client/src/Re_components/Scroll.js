e4871a61a6e6c6f44af97d5e41d1535f42251423
Shakiba448 <shakiba448@gmail.com>
2021-09-21 13:57:06 +0600

    WIP ON NEXT LENSE

--- client/src/Re_components/Scroll.js
+++ client/src/Re_components/Scroll.js
@@ -1,27 +1,25 @@
 import React, { useState } from "react";
 import { Box, Typography, Grid } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import { useSelector } from "react-redux";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 import productApi from "../../pages/api/products";
-import { productSelector } from "../redux/slices/productSlice";
 
-const Scroll = ({ scrollView, moreProduct, setMoreProduct }) => {
+const Scroll = ({ scrollView, state, setState }) => {
   const [hasMore, setHasMore] = useState(true);
 
   const getMoreProduct = async () => {
     const {
       data: { products },
-    } = await productApi.getMoreProducts(moreProduct.length, 4);
-    setMoreProduct((product) => [...product, ...products]);
-    if (moreProduct.length >= 42) {
+    } = await productApi.getMoreProducts(state.length, 14);
+    setState((product) => [...product, ...products]);
+    if (state.length >= 42) {
       setHasMore(false);
     }
   };
   return (
     <InfiniteScroll
-      dataLength={moreProduct.length}
+      dataLength={state.length}
       next={getMoreProduct}
       hasMore={hasMore}
       endMessage={
