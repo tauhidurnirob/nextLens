e8e268ee6f9105c250ed3e993c850f0cb54f0f03
Shakiba448 <shakiba448@gmail.com>
2021-09-22 18:04:16 +0600

    WIP ON NEXT LENSE

--- client/src/Re_components/CategoryProducts.js
+++ client/src/Re_components/CategoryProducts.js
@@ -60,7 +60,7 @@ const CategoryEyeGlassProducts = () => {
           <FilterByFrameShape />
           <FilterByShopCollection />
         </Grid>
-        {products.length <= 12 ? (
+        {/* {products.length <= 6 ? (
           <Grid
             direction="row"
             container
@@ -73,27 +73,27 @@ const CategoryEyeGlassProducts = () => {
               </Grid>
             ))}
           </Grid>
-        ) : (
-          <Grid
-            direction="row"
-            container
-            md={8}
-            className={clsx({ [classes.product]: products?.length <= 9 })}
-          >
-            <Scroll
-              pLength={products.length}
-              scrollView={
-                <Grid container direction="row">
-                  {products?.map((item) => (
-                    <Grid item key={item.id} container md={4}>
-                      <Cards item={item} isProduct width={400} height={400} />
-                    </Grid>
-                  ))}
-                </Grid>
-              }
-            />
-          </Grid>
-        )}
+        ) : ( */}
+        <Grid
+          direction="row"
+          container
+          md={8}
+          className={clsx({ [classes.product]: products?.length <= 9 })}
+        >
+          <Scroll
+            pLength={products.length}
+            scrollView={
+              <Grid container direction="row">
+                {products?.map((item) => (
+                  <Grid item key={item.id} container md={4}>
+                    <Cards item={item} isProduct width={400} height={400} />
+                  </Grid>
+                ))}
+              </Grid>
+            }
+          />
+        </Grid>
+        {/* )} */}
       </Grid>
     </Container>
   );
