7c9c7d3515e337fb04d7dfdf7edf7aaf03c25ee6
Shakiba448 <shakiba448@gmail.com>
2021-10-06 15:58:34 +0600

    WIP ON NEXT LENSE

--- admin-dashboard/src/Components/Customers/CustomerForm.js
+++ admin-dashboard/src/Components/Customers/CustomerForm.js
@@ -10,6 +10,12 @@ import {
   Grid,
 } from "@material-ui/core";
 import clsx from "clsx";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  adminProductSelector,
+  allProductAction,
+} from "../../redux/slices/productSlice";
 
 const useStyles = makeStyles((theme) => ({
   root: { padding: theme.spacing(2) },
@@ -23,8 +29,37 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const CustomerForm = () => {
+const CustomerForm = ({ searchFunc }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const { allProduct } = useSelector(adminProductSelector);
+
+  const handleSearchChange = (e) => {
+    searchFunc(e.target.value);
+  };
+
+  const handlePriceChange = (e) => {
+    let product = [...allProduct];
+    if (e.target.value === 0) {
+      let shuffled = product
+        .map((products) => ({ products, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ products }) => products);
+      dispatch(allProductAction(shuffled));
+    }
+    if (e.target.value === 1) {
+      product?.sort((a, b) => {
+        return b.price - a.price;
+      });
+      dispatch(allProductAction(product));
+    }
+    if (e.target.value === 2) {
+      product?.sort((a, b) => {
+        return a.price - b.price;
+      });
+      dispatch(allProductAction(product));
+    }
+  };
 
   return (
     <Paper className={clsx(classes.root)}>
@@ -43,6 +78,7 @@ const CustomerForm = () => {
                 label="Search"
                 variant="outlined"
                 name="search"
+                onChange={handleSearchChange}
               />
             </FormControl>
           </Grid>
@@ -53,8 +89,13 @@ const CustomerForm = () => {
               className={clsx(classes.formControl)}
             >
               <InputLabel id="price">Order</InputLabel>
-              <Select labelId="price" id="price" label="price">
-                <MenuItem value="">
+              <Select
+                onChange={handlePriceChange}
+                labelId="price"
+                id="price"
+                label="price"
+              >
+                <MenuItem value={0}>
                   <em>None</em>
                 </MenuItem>
                 {price?.map((item, index) => (
