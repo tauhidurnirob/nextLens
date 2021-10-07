96a439d8ce23b18a118f5cfbe45ccceec386acad
Shakiba448 <shakiba448@gmail.com>
2021-10-07 14:53:05 +0600

    WIP ON NEXT LENSE

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -3,12 +3,17 @@ import mongoose from "mongoose";
 const orderSchema = mongoose.Schema(
   {
     order_id: { type: String, required: true },
-    address: { type: String, required: true },
+    deliveryAddress: { type: String, required: true },
     paymentMethod: { type: String, required: true },
     status: { type: String, required: true },
-    customer: { type: String, required: true },
-    contact: { type: String, required: true },
-    price: { type: String, required: true },
+    history: [
+      {
+        date: { type: Date, default: Date.now },
+        customerId: { type: String, required: true },
+        contact: { type: String, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
   },
   {
     versionKey: false,
