55af04a1ab79911d2868e7cbfb6558572514271e
Shakiba448 <shakiba448@gmail.com>
2021-10-17 16:43:13 +0600

    WIP on Admin

--- admin-dashboard/src/api/endpoints.js
+++ admin-dashboard/src/api/endpoints.js
@@ -2,5 +2,6 @@ const endpoint = {
   post: "/api/products",
   allProducts: "/api/admin/products",
   orders: "/api/admin/order",
+  users: "/api/users",
 };
 export default endpoint;
--- admin-dashboard/src/redux/entities.js
+++ admin-dashboard/src/redux/entities.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import productsReducer from "./slices/productSlice";
 import orderReducer from "./slices/orderSlice";
+import userReducer from "./slices/userSlice";
 
 export default combineReducers({
   products: productsReducer,
   order: orderReducer,
+  user: userReducer,
 });
