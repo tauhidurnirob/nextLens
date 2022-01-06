9e33f0ec6580604edba5d4e8a5fe02a74d6df0e2
Shakiba448 <shakiba448@gmail.com>
2022-01-06 17:18:08 +0600

    WIP on Client

--- client/pages/api/client.js
+++ client/pages/api/client.js
@@ -1,8 +1,8 @@
 import { create } from "apisauce";
 
-const apiClient = create({ baseURL: "http://localhost:5000/" });
-// const apiClient = create({
-//   baseURL: "https://secure-everglades-15883.herokuapp.com/",
-// });
+// const apiClient = create({ baseURL: "http://localhost:5000/" });
+const apiClient = create({
+  baseURL: "https://secure-everglades-15883.herokuapp.com/",
+});
 
 export default apiClient;
